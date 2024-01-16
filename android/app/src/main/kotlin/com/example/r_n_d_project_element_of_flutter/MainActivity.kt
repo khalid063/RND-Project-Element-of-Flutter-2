14190780de0601986e45a4d610a50423ea8641c8
DeltaKhalid <khalid.delta063@gmail.com>
2024-01-16 11:35:44 +0600

    test second commit

---
+++ android/app/src/main/kotlin/com/example/r_n_d_project_element_of_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.r_n_d_project_element_of_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RND Project 2',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RND Project 2'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
+
+
